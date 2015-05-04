315ababe188c5510fe255915fe2d5f5100c724b1
Cimran Virdi <virdi@mit.edu>
2015-05-04 00:53:36 -0400

    added some final touches

--- Student.js
+++ Student.js
@@ -1,26 +1,33 @@
 /**
  * Student represents a student in the database
  *
- *
- *
  */
 
 var dict = {"Allie": "faces/Allie.png", "Joyce": "faces/Joyce.png", "Malika":"faces/Malika.png", "McCall":"faces/McCall.png", "Milly":"faces/Milly.png", "Sara":"faces/Sara.jpg", "Scotty": "faces/Scotty.png", "Eben": "faces/Eben.png"};
 
-var Student = function (First, Last, Gender, Learned, Undergrad) {
- 
+function Student(First, Last, Gender, Learned, Undergrad) {
+    this.fullName = First + " " + Last; 
     this.firstName = First;
     this.lastName = Last; 
     this.gender = Gender; 
     this.learned = Learned; //boolean
     this.undergrad = Undergrad; 
-    this.img = dict[firstName];  
+    this.img = dict[First];  
     
-    function LearnedName() { 
+    function learnedName() { 
         if (this.learned = false) {
             this.learned = true; 
+        }
     }
-    }
-}
+    
+  
+    };
+
         
+
+
+      
+      
+
+
     
